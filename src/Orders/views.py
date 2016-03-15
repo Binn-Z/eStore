ae7a303f91a920520f216e2aecabfc340bed9e6d
Binn <zhoubin9504@gmail.com>
2016-03-15 22:39:05 +0800

    test

--- src/Orders/views.py
+++ src/Orders/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from  django.views.generic.list import ListView
 from  django.views.generic.detail import DetailView
+from django.contrib.auth.models import User
 
 from .models import Orders
+from Customer.models import Customer
 # Create your views here.
 
 class Orders_ListView(ListView):
@@ -12,6 +14,16 @@ class Orders_ListView(ListView):
 	paginate_by=9
 
 	def get_context_data(self, **kwargs):
-		context = super(Orders_ListView, self).get_context_data(**kwargs)
-		context['object_list'] = Orders.objects.filter(o_Status__exact=1)
+		userid=self.request.user.id
+		print ( userid)
+		if  userid != None:
+			c_Phone = User.objects.get(id=userid).username
+			customer = Customer.objects.get(c_Phone=c_Phone)
+			context = super(Orders_ListView, self).get_context_data(**kwargs)
+			context['is_login'] = True
+			context['object_list'] = Orders.objects.filter(o_Status__exact=0,c_Id__exact=customer.id)
+		else:
+			context = super(Orders_ListView, self).get_context_data(**kwargs)
+			context['object_list'] = None
+			context['is_login'] = False
 		return context
